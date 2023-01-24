133b06f52d6f915e4d6f7b3c9c74a367560a9c3c
Matthieu Parent <matthieuparent@outlook.com>
2023-01-24 15:19:19 -0500

    working files

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+console.log("Mon javascript se charge. Tout fonctionne et je retire se console.log");
---
+++ src/scripts/Main.js
@@ -0,0 +1,3 @@
+console.log(
+  'Mon javascript se charge. Tout fonctionne et je retire se console.log'
+);
